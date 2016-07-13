0775763bb7b4473b8443aa662081ae6564ab1295
Raphael Scholer <rascholer@gmail.com>
2016-07-13 03:19:54 +0200

    st-git-local - Apply scrollback patch

--- local/st-git-local/config.h
+++ local/st-git-local/config.h
@@ -7,6 +7,7 @@
  */
 static char font[] = "Droid Sans Mono:size=10";
 static int borderpx = 2;
+#define histsize 1000
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -157,6 +158,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY|ShiftMask,     XK_C,           clipcopy,       {.i =  0} },
 	{ MODKEY|ShiftMask,     XK_V,           clippaste,      {.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
